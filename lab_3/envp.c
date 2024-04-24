ac8f500b2d894d70af527ce50ebc3f1610f444c2
Paul <quangphatthai@gmail.com>
2024-04-24 08:27:03 +0700

    envp

---
+++ lab_3/envp.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[], char **envp) {
+    int fd = open("shared_file.txt", O_RDWR | O_CREAT, 0666);
+    ftruncate(fd, 16);
+
+    char *mapm = mmap(NULL, 16, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (argc < 2) {
+        printf("USAGE: %s read | write", argv[0]);
+        return 0;
+    }
+    if (strcmp(argv[1], "read\0") == 0) {
+        printf("Mapped memory content: %s\n", mapm);
+    }
+    else if (strcmp(argv[1], "write\0") == 0) {
+        ftruncate(fd, 0);
+        ftruncate(fd, 16);
+        sprintf(mapm, "%s", argv[2]);
+    }
+    else {
+        printf("Command not support");
+    }
+
+    while (*envp) {
+        printf("%s\n", *envp++);
+    }
+    
+    close(fd);
+    return 0;
+}
+
